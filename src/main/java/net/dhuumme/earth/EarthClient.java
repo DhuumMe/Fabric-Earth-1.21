35495cbaa677d29240dce1155a535d61747be483
DhuumMe <benoit.abelard@gmail.com>
2024-08-15 16:18:25 +0200

    Initial commit

---
+++ src/main/java/net/dhuumme/earth/EarthClient.java
@@ -0,0 +1,10 @@
+package net.dhuumme.earth;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EarthClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/dhuumme/earth/EarthDataGenerator.java
@@ -0,0 +1,11 @@
+package net.dhuumme.earth;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class EarthDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
